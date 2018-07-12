e4282b3afcf30804207f8f0a52068474aa1a68c1
おいしゅん/Oishun <{shunoikawa1112@gmail.com}>
2018-07-12 20:56:10 +0900

    cryptozombiesから.sendあたりを移植しようと試み中

--- index.js
+++ index.js
@@ -6,9 +6,11 @@
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
 //contractInstance = VotingContract.at(/*'0xca2f65265357763c442c4e65eb43baa4d99fdd38'*/);
 
-
+var donateEtherAddress = "MY_CONTRACT_ADDRESS"; 
+donateEther = new web3js.eth.Contract(donateEtherABI, donateEtherAddress);
 
 ///////----------------frm (For donate)----------------------///////
+
 
 var twitterId = document.forms['frm'].elements['donator_twitterId'].value
 var amount = document.forms['frm'].elements['donator_amount'].value
@@ -16,6 +18,9 @@ var amount = document.forms['frm'].elements['donator_amount'].value
 document.getElementById("donate-button").onclick = function() {
   //document.getElementById("form-text").innerHTML = "こんにちは " + document.getElementById("TwitterID").value + " さん！";
   document.getElementById("form-text").innerHTML = document.forms['frm'].elements['donator_amount'].value + "etherを" + document.forms['frm'].elements['donator_twitterId'].value + " さんとして寄付します" ;
+
+  //--------------- メタマスク処理 ----------------//
+
 
   if (typeof web3 === 'undefined') {
     return alert('You need to install MetaMask to use this feature.')
@@ -25,15 +30,14 @@ document.getElementById("donate-button").onclick = function() {
     return alert('You need to log in MetaMask to use this feature.')
   }
 
-  web3.eth.sendTransaction({
-    to: "0x4612A97c3bF711F7048277d3642C68eac1148bF0",
-    from: user_address,
-    value: web3.toWei('0.005', 'ether'),
-  }, function (err, transactionHash) {
-    if (err) return alert('Thanks for trying out!');
-    alert('Thanks for the generosity!!');
-  }).then(contractInstance.pushDonator(twitterId, amount);)
-
+  return donateEther.methods.DonateAtion(twitterId, amount)
+  .send({ from: userAccount, value: web3.utils.toWei(amount, "ether") })
+  .on("receipt", function(receipt) {
+    $("#txStatus").text("Thank you for trying my DApp!!");
+  })
+  .on("error", function(error) {
+    $("#txStatus").text(error);
+  });
 
 };
 
